9cc2be6b98b5e8eb328daffa1b1ada4ac2df9f92
eliavamor1-collab <eliavamor1@gmail.com>
2026-08-17 16:10:04 +0300

    Auto-update: Mon 08/17/2026 16:10:04.33

--- commands/tiktok.js
+++ commands/tiktok.js
@@ -21,8 +21,10 @@ export default {
 ℹ️ *הערות:*
 פשוט להתקין ולהשתמש
 
-📲 *קישור להורדה:*
-https://liteapks.com/download/tiktok-81/1`;
+━━━━━━━━━━━━━━━
+⬇️ *לחץ להורדה ישירה* ⬇️
+https://liteapks.com/download/tiktok-81/1
+━━━━━━━━━━━━━━━`;
 
     try {
       if (savedMessage) {
