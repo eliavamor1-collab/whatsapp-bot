dc4dba8090f910e7be9f41d9d74c849f7b42108b
eliavamor1-collab <eliavamor1@gmail.com>
2026-08-17 16:10:09 +0300

    Auto-update: Mon 08/17/2026 16:10:09.52

--- commands/youtuberevanced.js
+++ commands/youtuberevanced.js
@@ -21,8 +21,10 @@ export default {
 ℹ️ *הערות:*
 פשוט להתקין ולהשתמש
 
-📲 *קישור להורדה:*
-https://liteapks.com/download/youtube-revanced-71686/1`;
+━━━━━━━━━━━━━━━
+⬇️ *לחץ להורדה ישירה* ⬇️
+https://liteapks.com/download/youtube-revanced-71686/1
+━━━━━━━━━━━━━━━`;
 
     try {
       if (savedMessage) {
