18a7d4f83fad9b6dc303d8d4d34ac19bd14bf1aa
eliavamor1-collab <eliavamor1@gmail.com>
2026-08-19 02:54:00 +0300

    Auto-update: Wed 08/19/2026  2:54:00.87

--- main.js
+++ main.js
@@ -59,6 +59,7 @@ import mxplayerCommand from "./commands/mxplayer.js";
 import flstudioCommand from "./commands/flstudio.js";
 import picretouchCommand from "./commands/picretouch.js";
 import sportCommand from "./commands/sport.js";
+import animefyCommand from "./commands/animefy.js";
 import { containsCurse, handleCurse } from "./commands/cursefilter.js";
 
 const { Pool } = pg;
