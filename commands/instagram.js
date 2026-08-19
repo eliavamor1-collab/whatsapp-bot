5243a29dde7b9996f1ac87c8659d1b4a5dd5e761
eliavamor1-collab <eliavamor1@gmail.com>
2026-08-19 06:26:09 +0300

    Auto-update: Wed 08/19/2026  6:26:08.98

--- commands/instagram.js
+++ commands/instagram.js
@@ -2,7 +2,7 @@ let savedMessage = null;
 
 export default {
   trigger: "instagram",
-  aliases: ["אינסטגרם", "אינסטה", "instagram"],
+  aliases: ["אינסטגרם", "instagram"],
 
   async execute(sock, message) {
     const jid = message.key.remoteJid;
