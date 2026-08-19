4c4d9c38946478c288d82f06b201a309f520a2ed
eliavamor1-collab <eliavamor1@gmail.com>
2026-08-19 06:15:43 +0300

    Auto-update: Wed 08/19/2026  6:15:42.92

--- commands/snaptube.js
+++ commands/snaptube.js
@@ -2,7 +2,7 @@ let savedMessage = null;
 
 export default {
   trigger: "snaptube",
-  aliases: ["סנאפטיוב", "סנאפ"],
+  aliases: ["סנאפטיוב", "snapchat", "סנפצ'אט"],
 
   async execute(sock, message) {
     const jid = message.key.remoteJid;
