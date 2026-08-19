ad43f649d2e5c655517fa4b2ad16743141c0dba6
eliavamor1-collab <eliavamor1@gmail.com>
2026-08-19 06:15:41 +0300

    Auto-update: Wed 08/19/2026  6:15:41.49

--- commands/mxplayer.js
+++ commands/mxplayer.js
@@ -2,7 +2,7 @@ let savedMessage = null;
 
 export default {
   trigger: "mx player",
-  aliases: ["מקס פלייר", "mx"],
+  aliases: ["מקס פלייר"],
 
   async execute(sock, message) {
     const jid = message.key.remoteJid;
