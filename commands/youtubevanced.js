2b43eb469e67ecdd2f84c2909f371425f38d0bf3
eliavamor1-collab <eliavamor1@gmail.com>
2026-08-19 05:08:03 +0300

    Auto-update: Wed 08/19/2026  5:08:03.66

--- commands/youtubevanced.js
+++ commands/youtubevanced.js
@@ -2,7 +2,7 @@ let savedMessage = null;
 
 export default {
   trigger: "youtube vanced",
-  aliases: ["יוטיוב ונסד", "vanced", "ונסד"],
+  aliases: ["יוטיוב ונסד", "youtube vanced", "vanced", "ונסד"],
 
   async execute(sock, message) {
     const jid = message.key.remoteJid;
