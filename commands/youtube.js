b8660cc95f54b95a3bf0ffa70e049413fe8dd612
eliavamor1-collab <eliavamor1@gmail.com>
2026-09-11 16:28:53 +0300

    Update YouTube Morphe links and tutorial video

--- commands/youtube.js
+++ commands/youtube.js
@@ -8,11 +8,11 @@ export default createApp({
   size: "17.64 MB",
   type: "סטרימינג וידאו",
   content: "פלטפורמת וידאו עולמית שבה תוכלו ליצור, לשתף סרטונים ולצפות בתכנים עם מיליארדי משתמשים ברחבי העולם.",
-  notes: "עיקבו בדיוק אחר הוראות ההתקנה בסירטון פה:\nhttps://youtube.com/shorts/y3WKhi2EfOU?si=TJVx-UNczW0RhuvJ",
+  notes: "עיקבו בדיוק אחר הוראות ההתקנה בסירטון פה:\nhttps://www.youtube.com/watch?v=Z-nYCyhr0no",
   image: "https://images.icon-icons.com/1488/PNG/512/5295-youtube-i_102568.png",
   links: [
-    { label: "⬇️ *לחץ להורדת Morphe* ⬇️", url: "https://morphe.software/" },
-    { label: "📦 *לחץ להורדת Micro G* 📦", url: "https://drive.google.com/file/d/1G-sWLiTa5eIoGwxVsWH4sDjZWEgnbRUm/view?usp=sharing" },
+    { label: "⬇️ *לחץ להורדת יוטיוב מורפ* ⬇️", url: "https://drive.google.com/file/d/1-APF6dib6tWzp5FHS-GzZBWZRSEU5RrD/view?usp=drivesdk" },
+    { label: "📦 *לחץ להורדת Micro G* 📦", url: "https://drive.google.com/file/d/1mK9WUhQlv_NBefwTH8lXvrR5RgqM4t_o/view?usp=drivesdk" },
     { label: "⚙️ *לחץ להורדת הגדרות ליוטיוב מורפ* ⚙️", url: "https://drive.google.com/file/d/1AV3MrVokAxK1t20qRPLeZ3DkqSTlKwEH/view?usp=sharing" }
   ]
 });
