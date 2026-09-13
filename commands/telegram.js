75b1ffc38fb541c385a9f689ca72fb1524922ff2
eliavamor1-collab <eliavamor1@gmail.com>
2026-09-14 00:45:14 +0300

    Update Telegram image, link and version to 9mod

--- commands/telegram.js
+++ commands/telegram.js
@@ -4,11 +4,11 @@ export default createApp({
   trigger: "telegram",
   aliases: ["טלגרם"],
   name: "Telegram",
-  version: "v12.9.2",
-  size: "67 MB",
+  version: "v12.10.1",
+  size: "85 MB",
   type: "מסנג'ר",
   content: "מסנג'ר מהיר ומאובטח עם פרימיום — קבוצות ענק, ערוצים, שליחת קבצים גדולים ועוד — ללא פרסומות.",
   notes: "⚠️ *שימו לב!*\nלפני שמוחקים את טלגרם, מומלץ להגדיר דרך כניסה נוספת כדי להימנע מתלות ב־SMS.\nהיכנסו ל:\n*הגדרות → פרטיות ואבטחה → אימות דו־שלבי*, והגדירו את האימות שלכם\nומומלץ גם להגדיר מפתח גישה (Passkey).\nשימו לב: אם טלגרם כבר דורשים תשלום עבור שליחת SMS, אין דרך לעקוף את החיוב, סליחה.🙏",
-  image: "https://liteapks.com/wp-content/uploads/2022/04/telegram-150x150.png",
-  links: ["https://liteapks.com/download/telegram-810/1"]
+  image: "https://9mod.com/wp-content/uploads/2024/06/telegram-150x150.webp",
+  links: ["https://9mod.com/download/telegram-1051/2"]
 });
